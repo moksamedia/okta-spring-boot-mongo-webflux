f67e07999e062335f50cb49ff5ecc38af1e5c511
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-12-05 09:56:26 -0800

    created start branch

--- src/main/java/com/okta/springbootmongo/KayakRepository.java
+++
@@ -1,8 +0,0 @@
-package com.okta.springbootmongo;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import reactor.core.publisher.Flux;
-
-public interface KayakRepository extends ReactiveMongoRepository<Kayak, Long> {
-    Flux<Kayak> findAll();
-}

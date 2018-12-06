ff4d96941e5f7ce6b50d00b1d6d8553bc1fdffc0
Andrew Hughes <andrewcarterhughes@gmail.com>
2018-12-05 19:28:53 -0800

    auth without groups

--- src/main/java/com/okta/springbootmongo/SecurityConfiguration.java
+++ src/main/java/com/okta/springbootmongo/SecurityConfiguration.java
@@ -1,51 +1,23 @@
 package com.okta.springbootmongo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 @EnableWebFluxSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration {
   
   @Bean
   public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
     http
         .authorizeExchange()
-        .pathMatchers(HttpMethod.POST, "/kayaks/**").hasAuthority("Admin")
         .anyExchange().authenticated()
         .and()
         .oauth2ResourceServer()
-        .jwt()
-        .jwtAuthenticationConverter(grantedAuthoritiesExtractor());
+        .jwt();
     return http.build();
-  }
-
-
-  static class GrantedAuthoritiesExtractor extends JwtAuthenticationConverter {
-    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-      Collection<String> authorities = (Collection<String>)jwt.getClaims().get("groups");
-      return authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-    }
-  }
-
-  Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
-    GrantedAuthoritiesExtractor extractor = new GrantedAuthoritiesExtractor();
-    return new ReactiveJwtAuthenticationConverterAdapter(extractor);
   }
   
 }

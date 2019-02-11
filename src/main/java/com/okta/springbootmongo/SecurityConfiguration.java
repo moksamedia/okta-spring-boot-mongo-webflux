6bb6e0e059fdc50a153bbf10d5334ae49102927c
Andrew Hughes <andrewcarterhughes@gmail.com>
2019-02-11 10:05:44 -0800

    updated for okta starter 1.1.0

--- src/main/java/com/okta/springbootmongo/SecurityConfiguration.java
+++ src/main/java/com/okta/springbootmongo/SecurityConfiguration.java
@@ -1,22 +1,11 @@
 package com.okta.springbootmongo;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
-
-import java.util.Collection;
-import java.util.stream.Collectors;
 
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
